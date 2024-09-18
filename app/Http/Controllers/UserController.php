603a336246d089ff45e88f4622c7969e1d2faa3f
Ahmad Hidayat <ahmadhidayatpermana202@gmail.com>
2024-09-18 09:17:51 +0700

    Jobsheet 4 - Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
-    public function index(){
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        userModel::where('username', 'customer-1')->update($data);
 
-        $user = UserModel::all();
+    public function index(){
+        $user = UserModel::where('username','manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'user_id';
     protected $table = 'm_user';
-    protected $primarykey = 'user_id';
+
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
