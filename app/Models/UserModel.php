6e8733c491a0626d135a81c65a74128b6bdcc8e8
AhmadHidayatP <ahmadhidayatpermana202@gmail.com>
2024-11-05 17:03:45 +0700

    Jobsheet 10 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,14 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
     use HasFactory;
 
     protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendfinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['username', 'password', 'nama', 'level_id','created_at', 'updated_at'];
+    protected $fillable = ['username', 'password','avatar', 'nama', 'level_id','created_at', 'updated_at'];
     protected $hidden = ['password'];
     protected $casts = ['password' => 'hashed'];
 
